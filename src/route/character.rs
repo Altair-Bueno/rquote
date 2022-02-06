8e65a959623b0e90f3e805730472458c37808536
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-02-06 20:08:47 +0100

    new: Character page

--- src/route/character.rs
+++ src/route/character.rs
@@ -1,6 +1,8 @@
 use yew::prelude::*;
 
+use crate::AnimechanQuote;
 use crate::component::nav_bar::*;
+use crate::component::quote::*;
 
 #[derive(Properties, PartialEq, Clone)]
 pub struct CharacterProp {
@@ -9,7 +11,43 @@ pub struct CharacterProp {
 
 #[function_component(Character)]
 pub fn home(character_prop: &CharacterProp) -> Html {
+    let client = use_context::<crate::context::Context>()
+        .expect("no ctx found")
+        .client()
+        .clone();
+    let quotes = use_state(|| Ok(vec![]));
+    let character = character_prop.character.to_string();
+    {
+        let quotes = quotes.clone();
+        let client = client.clone();
+        let character = character.clone();
+        use_effect_with_deps(move |_| {
+            let client = client.clone();
+            let quotes = quotes.clone();
+            let character = character.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                let response = AnimechanQuote::get_quote_character(&client, character.as_str(), None).await;
+                quotes.set(response);
+            });
+            || ()
+        }, ());
+    };
+    let html = match quotes.as_ref() {
+        Ok(quotes) => {
+            quotes
+                .iter()
+                .map(|x| html! {
+                    <QuoteComponent quote = {Box::new(x.clone())}/>
+                })
+                .collect::<Html>()
+        }
+        Err(err) => {
+            html! {
+                <p>{format!("Something has happened: {err}")}</p>
+            }
+        }
+    };
     html! {
-        <p>{character_prop.character.as_str()}</p>
+        {html}
     }
 }
