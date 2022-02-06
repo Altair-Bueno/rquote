c9a9fe77f82e2952f829994c7fc83767a7d2d49e
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-02-06 19:24:06 +0100

    new: context for yew components

---
+++ src/context.rs
@@ -0,0 +1,22 @@
+use reqwest::Client;
+
+#[derive(Clone, Debug)]
+pub struct Context {
+    client: Client,
+}
+
+impl PartialEq for Context {
+    fn eq(&self, other: &Self) -> bool {
+        true
+    }
+}
+
+impl Context {
+    pub fn new() -> Context {
+        Context { client: Client::new() }
+    }
+
+    pub fn client(&self) -> &Client {
+        &self.client
+    }
+}
