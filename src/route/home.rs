67828ac488ade460dd42e9d3714dfd363755507c
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-02-07 20:36:55 +0100

    ref: Home page now can emit futures using callbacks

--- src/route/home.rs
+++ src/route/home.rs
@@ -1,48 +1,92 @@
-use reqwest::Error;
+use reqwest::{Client, Error};
 use yew::prelude::*;
 
 use crate::AnimechanQuote;
+use crate::component::loading::*;
 use crate::component::quote::*;
 
-#[function_component(Home)]
-pub fn home() -> Html {
-    let client = use_context::<crate::context::Context>()
-        .unwrap_or_default()
-        .client()
-        .clone();
-    let quotes = use_state(|| Ok(vec![]));
-    {
-        let quotes = quotes.clone();
-        let client = client.clone();
-        use_effect_with_deps(
-            move |_| {
-                let client = client.clone();
-                let quotes = quotes.clone();
-                wasm_bindgen_futures::spawn_local(async move {
-                    let response = AnimechanQuote::get_10_random_quotes(&client).await;
-                    quotes.set(response);
-                });
-                || ()
-            },
-            (),
-        );
-    };
-    let html = match quotes.as_ref() {
-        Ok(quotes) => quotes
+#[derive(Debug)]
+pub enum Message {
+    Loading,
+    Successful(Vec<AnimechanQuote>),
+    Failed(reqwest::Error),
+}
+
+impl Default for Message {
+    fn default() -> Self {
+        Message::Loading
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Home {
+    quotes: Message,
+}
+
+impl Home {
+    async fn fetch_data(client: &Client) -> Message {
+        let response = AnimechanQuote::get_10_random_quotes(&client).await;
+        return match response {
+            Ok(x) => Message::Successful(x),
+            Err(x) => Message::Failed(x)
+        };
+    }
+
+    fn loading_view() -> Html {
+        html! {<LoadingComponent/>}
+    }
+    fn successful_view(quotes: &[AnimechanQuote]) -> Html {
+        quotes
             .iter()
-            .map(|x| {
-                html! {
-                    <QuoteComponent quote = {Box::new(x.clone())}/>
-                }
+            .map(|x| html! {
+                <QuoteComponent quote = {x.clone()}/>
             })
-            .collect::<Html>(),
-        Err(err) => {
-            html! {
-                <p>{format!("Something has happened: {err}")}</p>
-            }
+            .collect()
+    }
+    fn failed_view(error: &reqwest::Error) -> Html {
+        html! {
+            {"Something happened. Please refresh the page"}
         }
-    };
-    html! {
-        {html}
+    }
+}
+
+impl Component for Home {
+    type Message = Message;
+    type Properties = ();
+
+    fn create(ctx: &Context<Self>) -> Self {
+        let client = ctx
+            .link()
+            .context::<crate::context::Context>(Default::default())
+            .map(|x| x.0)
+            .unwrap_or_default()
+            .client()
+            .clone();
+        ctx
+            .link()
+            .callback_future_once(|_| async move {
+                Home::fetch_data(&client).await
+            })
+            .emit(());
+        Home::default()
+    }
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        self.quotes = msg;
+        true
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let client = ctx
+            .link()
+            .context::<crate::context::Context>(Default::default())
+            .map(|x| x.0)
+            .unwrap_or_default()
+            .client()
+            .clone();
+        match &self.quotes {
+            Message::Loading => Home::loading_view(),
+            Message::Successful(x) => Home::successful_view(x),
+            Message::Failed(x) => Home::failed_view(x),
+        }
     }
 }
