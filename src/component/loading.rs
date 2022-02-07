f237f6aa809e7539a244e0fbd8b7cd7529320d67
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-02-07 14:05:38 +0100

    fix: LoadingComponent was not public

--- src/component/loading.rs
+++ src/component/loading.rs
@@ -1,7 +1,7 @@
 use yew::prelude::*;
 
 #[function_component(LoadingComponent)]
-fn loading() -> Html {
+pub fn loading() -> Html {
     html! {
         <div class = "text-center m-1">
             <div class="spinner-border spinner-border-sm ms-auto m-1" role="status" aria-hidden="true"/>
