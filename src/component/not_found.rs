3b54867a8801d7744247251d6f4d19aa994a3631
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-02-07 15:13:12 +0100

    new: Not found now is router agnostic

--- src/component/not_found.rs
+++ src/component/not_found.rs
@@ -1,35 +1,68 @@
+use std::marker::PhantomData;
+
 use yew::prelude::*;
 use yew_router::prelude::*;
 
 use crate::route::Route;
 
 #[derive(Properties, PartialEq, Clone)]
-pub struct NotFoundProp {
+pub struct NotFoundProp<T>
+    where
+        T: Clone + Routable + PartialEq + 'static
+{
     #[prop_or_default]
     pub message: Option<String>,
+    #[prop_or_default]
+    pub home: Option<T>,
 }
 
-#[function_component(NotFoundComponent)]
-pub fn not_found(props: &NotFoundProp) -> Html {
-    let head = "Page Not Found";
-    let message = html! {
-        <p>{
-            props
-                .message
-                .as_ref()
-                .map(|x| x.as_str())
-                .unwrap_or("We could't find the page you requested")
-            }
-        </p>
-    };
-    let help = html! {
-        <Link<Route> to = {Route::Home}>{format!("Return to home")}</Link<Route>>
-    };
-    html! {
-        <div class="position-absolute top-50 start-50 translate-middle">
-            <h1>{head}</h1>
-            {message}
-            {help}
-        </div>
+pub struct NotFoundComponent<T>
+    where
+        T: Clone + Routable + PartialEq + 'static
+{
+    phantom: PhantomData<&'static T>,
+}
+
+impl<T> Component for NotFoundComponent<T>
+    where
+        T: Clone + Routable + PartialEq + 'static
+{
+    type Message = ();
+    type Properties = NotFoundProp<T>;
+
+    fn create(ctx: &Context<Self>) -> Self {
+        NotFoundComponent {
+            phantom: Default::default()
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let props = ctx.props();
+        let head = "Page Not Found";
+        let message = html! {
+            <p>{
+                props
+                    .message
+                    .as_ref()
+                    .map(|x| x.as_str())
+                    .unwrap_or("We couldn't find the page you requested")
+                }
+            </p>
+        };
+        let help = if let Some(home) = &props.home {
+            html! {<Link<T> to = {home.clone()}>{format!("Return to home")}</Link<T>>}
+        } else {
+            Html::default()
+        };
+        let help = html! {
+
+        };
+        html! {
+            <div class={classes!("position-absolute","top-50","start-50","translate-middle")}>
+                <h1>{head}</h1>
+                {message}
+                {help}
+            </div>
+        }
     }
 }
--- src/route/not_found.rs
+++ src/route/not_found.rs
@@ -2,10 +2,11 @@ use yew::prelude::*;
 
 use crate::component::nav_bar::*;
 use crate::component::not_found::*;
+use crate::Route;
 
 #[function_component(NotFound)]
 pub fn not_found() -> Html {
     html! {
-        <NotFoundComponent/>
+        <NotFoundComponent<Route> home={Some(Route::Home)}/>
     }
 }
