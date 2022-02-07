7526a07fe626ca972c9c361b3c567eb410150ed9
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-02-07 13:33:48 +0100

    ref: Switch NotFoundComponent  from Struct component to  function component

--- src/component/not_found.rs
+++ src/component/not_found.rs
@@ -9,38 +9,27 @@ pub struct NotFoundProp {
     pub message: Option<String>,
 }
 
-pub struct NotFoundComponent;
-
-impl Component for NotFoundComponent {
-    type Message = ();
-    type Properties = NotFoundProp;
-
-    fn create(ctx: &Context<Self>) -> Self {
-        NotFoundComponent
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let head = "Page Not Found";
-        let message = html! {
-            <p>{
-                ctx
-                    .props()
-                    .message
-                    .as_ref()
-                    .map(|x| x.as_str())
-                    .unwrap_or("We could't find the page you requested")
-                }
-            </p>
-        };
-        let help = html! {
-            <Link<Route> to = {Route::Home}>{format!("Return to home")}</Link<Route>>
-        };
-        html! {
-            <div class="position-absolute top-50 start-50 translate-middle">
-                <h1>{head}</h1>
-                {message}
-                {help}
-            </div>
-        }
+#[function_component(NotFoundComponent)]
+pub fn not_found(props: &NotFoundProp) -> Html {
+    let head = "Page Not Found";
+    let message = html! {
+        <p>{
+            props
+                .message
+                .as_ref()
+                .map(|x| x.as_str())
+                .unwrap_or("We could't find the page you requested")
+            }
+        </p>
+    };
+    let help = html! {
+        <Link<Route> to = {Route::Home}>{format!("Return to home")}</Link<Route>>
+    };
+    html! {
+        <div class="position-absolute top-50 start-50 translate-middle">
+            <h1>{head}</h1>
+            {message}
+            {help}
+        </div>
     }
 }
