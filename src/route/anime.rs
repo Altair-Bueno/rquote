b32cc4d19a24d24f716dee7594695cd39a760401
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-02-09 14:20:12 +0100

    new: Anime page done

--- src/route/anime.rs
+++ src/route/anime.rs
@@ -1,14 +1,83 @@
+use std::error::Error;
+use std::rc::Rc;
+
+use async_trait::async_trait;
+use reqwest::Client;
 use yew::prelude::*;
+
+use crate::animechan::AnimechanQuote;
+use crate::component::async_list::*;
+use crate::component::async_list::ViewAsyncListComponent;
+use crate::component::error::*;
+use crate::component::loading::*;
+use crate::component::quote::*;
 
 #[derive(Properties, PartialEq, Clone)]
 pub struct AnimeProp {
     pub title: String,
 }
 
-#[function_component(Anime)]
-pub fn anime(anime_prop: &AnimeProp) -> Html {
-    let title = anime_prop.title.as_str();
-    html! {
-        <p>{title}</p>
+#[derive(Debug, PartialEq, Clone)]
+pub struct Anime {
+    title: String,
+    page: Option<u32>,
+}
+
+#[async_trait(? Send)]
+impl ViewAsyncListComponent<AnimechanQuote> for Anime {
+    async fn fetch_data(&self, client: Client) -> Message<AnimechanQuote> {
+        let response = AnimechanQuote::get_quote_title(&client, &self.title, self.page).await;
+        match response {
+            Ok(x) => Message::Successful(x),
+            Err(err) => Message::Failed(Rc::new(err)),
+        }
+    }
+
+    fn successful_view(
+        &self,
+        ctx: &Context<AsyncListComponent<AnimechanQuote, Self>>,
+        quotes: &[AnimechanQuote],
+    ) -> Html {
+        quotes
+            .iter()
+            .map(|x| html! {
+                <QuoteComponent quote = {x.clone()} header = {false}/>
+            })
+            .collect()
+    }
+    fn failed_view(&self, ctx: &Context<AsyncListComponent<AnimechanQuote, Self>>, error: Rc<dyn Error>) -> Html {
+        let onclick = |_| todo!();
+        let _ = html! {
+            <button {onclick} class={classes!("btn","btn-light","text-dark")}>
+                {"Reload"}
+            </button>
+        };
+        html! {
+            <ErrorComponent severity={Severity::Danger} {error}>
+                //{reload_button}
+            </ErrorComponent>
+        }
+    }
+    fn loading_view(&self, ctx: &Context<AsyncListComponent<AnimechanQuote, Self>>) -> Html {
+        html! {<LoadingComponent/>}
+    }
+}
+
+impl Component for Anime {
+    type Message = ();
+    type Properties = AnimeProp;
+
+    fn create(ctx: &Context<Self>) -> Self {
+        Anime { title: ctx.props().title.clone(), page: None }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let title = self.title.as_str();
+        html! {
+            <>
+                <h1 class = {classes!("ms-5","my-3")}>{title}</h1>
+                <AsyncListComponent<AnimechanQuote,Self> provider={self.clone()}/>
+            </>
+        }
     }
 }
