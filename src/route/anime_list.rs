4f2ae5894ab3fc3a638cccd29afee34bf6ba7f73
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-02-09 14:49:59 +0100

    new: Anime list

--- src/route/anime_list.rs
+++ src/route/anime_list.rs
@@ -1,8 +1,89 @@
-use yew::prelude::*;
+use std::error::Error;
+use std::rc::Rc;
 
-#[function_component(AnimeList)]
-pub fn anime_list() -> Html {
-    html! {
-        <p>{"Anime list"}</p>
+use async_trait::async_trait;
+use reqwest::Client;
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+use crate::animechan::AnimechanQuote;
+use crate::component::async_list::*;
+use crate::component::async_list::ViewAsyncListComponent;
+use crate::component::error::*;
+use crate::component::loading::*;
+use crate::component::quote::*;
+use crate::route::Route;
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct AnimeList;
+
+#[async_trait(? Send)]
+impl ViewAsyncListComponent<String> for AnimeList {
+    async fn fetch_data(&self, client: Client) -> Message<String> {
+        let response = AnimechanQuote::get_anime_list(&client).await;
+        match response {
+            Ok(x) => Message::Successful(x),
+            Err(err) => Message::Failed(Rc::new(err)),
+        }
+    }
+
+    fn successful_view(&self, ctx: &Context<AsyncListComponent<String, Self>>, quotes: &[String]) -> Html {
+        let theme = ctx
+            .link()
+            .context::<crate::context::Theme>(Default::default())
+            .map(|x| x.0)
+            .unwrap_or_default();
+        let elements = quotes
+            .iter()
+            .map(|x| {
+                let route = Route::Anime { title: x.clone() };
+                html! {
+                <li class = {classes!("list-group-item")}>
+                    <Link<Route> to={route} classes={classes!(theme.get_link_class())}>
+                        {x}
+                    </Link<Route>>
+                </li>
+            }
+            });
+        html! {
+            <ul class = {classes!("list-group","m-3")}>
+                {for elements}
+            </ul>
+        }
+    }
+
+    fn failed_view(&self, ctx: &Context<AsyncListComponent<String, Self>>, error: Rc<dyn Error>) -> Html {
+        let onclick = |_| todo!();
+        let _ = html! {
+            <button {onclick} class={classes!("btn","btn-light","text-dark")}>
+                {"Reload"}
+            </button>
+        };
+        html! {
+            <ErrorComponent severity={Severity::Danger} {error}>
+                //{reload_button}
+            </ErrorComponent>
+        }
+    }
+
+    fn loading_view(&self, ctx: &Context<AsyncListComponent<String, Self>>) -> Html {
+        html! {<LoadingComponent/>}
+    }
+}
+
+impl Component for AnimeList {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        AnimeList
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <>
+                <AsyncListComponent<String,Self> provider={self.clone()}/>
+            </>
+        }
     }
 }
