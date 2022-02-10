d2f4a0c7dc2cc716a25f6af01e6213567f6c4dae
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-02-10 22:18:48 +0100

    new: Switch to python script for building index.html

---
+++ template.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import chevron
+
+__INDEX_HTML = 'index.html'
+__INDEX_MUSTACHE = 'templates/index.html.mustache'
+__DIST_FOLDER = 'dist'
+__DOCS_FOLDER = 'docs'
+
+data = {
+    "title": "Rquote",
+    "url": 'https://rquote.vercel.app',
+    "description": 'Web client for animechan API',
+    'logo': '',
+    'author': 'Altair-Bueno',
+    'theme': {
+        'dark': '202528',
+        'light': 'F8F9FA'
+    }
+}
+
+template = open(__INDEX_MUSTACHE, 'r')
+rendered = chevron.render(template, data)
+out = open('index.html', 'w')
+out.write(rendered)
+out.close()
