a065f7c1a8e3b8bc5ee0956012f610c4bd27ce21
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-02-11 20:08:03 +0100

    fix: Switch to Functional NotFoundComponent to fix theme repaint

--- crates/rquote-component/src/not_found.rs
+++ crates/rquote-component/src/not_found.rs
@@ -19,56 +19,35 @@ where
     pub info: String,
 }
 
-pub struct NotFoundComponent<T>
-where
-    T: Clone + Routable + PartialEq + 'static,
+
+#[function_component(NotFoundComponent)]
+pub fn not_found<T>(props: &NotFoundProp<T>) -> Html
+    where
+        T: Clone + Routable + PartialEq + 'static,
 {
-    phantom: PhantomData<&'static T>,
-}
-
-impl<T> Component for NotFoundComponent<T>
-where
-    T: Clone + Routable + PartialEq + 'static,
-{
-    type Message = ();
-    type Properties = NotFoundProp<T>;
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        NotFoundComponent {
-            phantom: Default::default(),
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let theme = ctx
-            .link()
-            .context::<Theme>(Default::default())
-            .map(|x| x.0)
-            .unwrap_or_default();
-        let props = ctx.props();
-        let text_class = theme.get_text_class();
-        let div_class = classes!(
-            text_class,
-            theme.get_background_class(),
-            // Center
-            "position-absolute",
-            "top-50",
-            "start-50",
-            "translate-middle",
-            // Shadow
-            "shadow-lg",
-            "p-3",
-            "mb-3",
-            "rounded"
-        );
-        html! {
-            <div class={div_class}>
-                <h1>{&props.title}</h1>
-                <p>{&props.message}</p>
-                <Link<T> to = {props.to.clone()} classes={classes!(theme.get_link_class())}>
-                    <strong>{&props.info}</strong>
-                </Link<T>>
-            </div>
-        }
+    let theme = use_context::<Theme>().unwrap_or_default();
+    let text_class = theme.get_text_class();
+    let div_class = classes!(
+        text_class,
+        theme.get_background_class(),
+        // Center
+        "position-absolute",
+        "top-50",
+        "start-50",
+        "translate-middle",
+        // Shadow
+        "shadow-lg",
+        "p-3",
+        "mb-3",
+        "rounded"
+    );
+    html! {
+        <div class={div_class}>
+            <h1>{&props.title}</h1>
+            <p>{&props.message}</p>
+            <Link<T> to = {props.to.clone()} classes={classes!(theme.get_link_class())}>
+                <strong>{&props.info}</strong>
+            </Link<T>>
+        </div>
     }
 }
