7881fe876662b519dab041ccb22f60c2e3bf0542
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-03-13 21:23:38 +0100

    fix: #25

--- src/route/anime.rs
+++ src/route/anime.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::rc::Rc;
 
 use reqwest::Client;
 use yew::prelude::*;
-use yew_hooks::use_async;
+use yew_hooks::{use_async, use_search_param};
+use yew_router::prelude::*;
 
 use rquote_component::error::*;
 use rquote_component::loading::*;
@@ -13,14 +15,15 @@ use rquote_core::AnimechanQuote;
 use rquote_core::wrapper::ClientWrapper;
 
 use crate::custom::quote::*;
+use crate::Route;
 
 #[derive(Properties, PartialEq, Clone)]
 pub struct AnimeProp {
     pub title: String,
 }
 
-async fn fetch_data(client:&Client,title:&str,page:Option<u32>) ->Result<Vec<AnimechanQuote>,Rc<dyn Error>> {
-    let response = AnimechanQuote::get_quote_title(client,title,page).await;
+async fn fetch_data(client: &Client, title: &str, page: Option<u32>) -> Result<Vec<AnimechanQuote>, Rc<dyn Error>> {
+    let response = AnimechanQuote::get_quote_title(client, title, page).await;
     match response {
         Ok(quote) => Ok(quote),
         Err(error) => {
@@ -35,37 +38,43 @@ pub fn view(props: &AnimeProp) -> Html {
     let theme = use_context::<Theme>().unwrap_or_default();
     let client = use_context::<ClientWrapper>().unwrap_or_default();
     let title = props.title.as_str();
-    let page = use_state(|| 0u32);
+    let init = use_search_param("page".to_string()).unwrap_or_default().parse::<u32>().unwrap_or_default();
+    let page = use_state(|| init);
 
     let next = {
         let page = page.clone();
-        Some(Callback::from(move |_:MouseEvent|page.set(*page + 1)))
+        Some(Callback::from(move |_: MouseEvent| page.set(*page + 1)))
     };
-    let prev = if *page == 0 { None }
-        else {
-            let page = page.clone();
-            Some(Callback::from(move |_:MouseEvent|page.set(*page -1)))
-        };
+    let prev = if *page == 0 { None } else {
+        let page = page.clone();
+        Some(Callback::from(move |_: MouseEvent| page.set(*page - 1)))
+    };
 
     let fetcher = {
         let page = page.clone();
         let title = props.title.clone();
-        use_async(async move {fetch_data(client.as_ref(),title.as_str(),Some(*page)).await})
+        use_async(async move { fetch_data(client.as_ref(), title.as_str(), Some(*page)).await })
     };
 
     {
         let page = page.clone();
         let fetcher = fetcher.clone();
-        use_effect_with_deps(move |_|{
+        let title = title.to_string();
+        let history = use_history().unwrap();
+        use_effect_with_deps(move |page| {
+            history.push_with_query(
+                Route::Anime { title },
+                HashMap::from([("page", **page)]),
+            ).unwrap();
             fetcher.run();
-            ||()
-        },page)
+            || ()
+        }, page)
     }
 
-    let content =if fetcher.loading {
+    let content = if fetcher.loading {
         html! {<LoadingComponent/>}
     } else if let Some(data) = &fetcher.data {
-        data.iter().map(|x| html!{
+        data.iter().map(|x| html! {
             <QuoteComponent quote = {x.clone()} header = {false} class = {classes!("m-3")}/>
         }).collect()
     } else if let Some(error) = &fetcher.error {
@@ -74,7 +83,7 @@ pub fn view(props: &AnimeProp) -> Html {
         html! {<ErrorComponent {severity} {error}/>}
     } else { Default::default() };
 
-    html!{
+    html! {
         <>
             <h1 class = {classes!("ms-3","my-3",theme.get_text_class())}>
                     {title}
